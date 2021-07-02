96cf39acec92b5ce457a948315bcbb173502c6ea
Peshkov <peshkovdenis1985@gmail.com>
2021-07-02 15:57:36 +0300

    added InsertionSort

---
+++ src/main/java/com/github/denpeshkov/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,15 @@
+package com.github.denpeshkov.algorithms.sorting;
+
+public class InsertionSort {
+  public static <T extends Comparable<? super T>> void sort(T[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      T key = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j].compareTo(key) > 0) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = key;
+    }
+  }
+}
