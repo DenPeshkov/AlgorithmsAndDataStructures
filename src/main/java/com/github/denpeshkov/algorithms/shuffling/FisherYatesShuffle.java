c62adb3c03277b1d1ceebdb67f5ae6b9c6074c76
Denis Peshkov <denpeshkov98@gmail.com>
2021-07-06 00:04:21 +0300

    google style

--- src/main/java/com/github/denpeshkov/algorithms/shuffling/FisherYatesShuffle.java
+++ src/main/java/com/github/denpeshkov/algorithms/shuffling/FisherYatesShuffle.java
@@ -3,15 +3,15 @@ package com.github.denpeshkov.algorithms.shuffling;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FisherYatesShuffle {
-    static <T> void shuffle(T[] arr) {
-        int N = arr.length;
+  static <T> void shuffle(T[] arr) {
+    int N = arr.length;
 
-        for (int i = 0; i < N; i++) {
-            int r = ThreadLocalRandom.current().nextInt(i + 1);
+    for (int i = 0; i < N; i++) {
+      int r = ThreadLocalRandom.current().nextInt(i + 1);
 
-            T t = arr[i];
-            arr[i] = arr[r];
-            arr[r] = t;
-        }
+      T t = arr[i];
+      arr[i] = arr[r];
+      arr[r] = t;
     }
+  }
 }
--- src/main/java/com/github/denpeshkov/algorithms/sorting/MergeSort.java
+++ src/main/java/com/github/denpeshkov/algorithms/sorting/MergeSort.java
@@ -1,28 +1,29 @@
 package com.github.denpeshkov.algorithms.sorting;
 
 public class MergeSort {
-    public static <T extends Comparable<? super T>> void sort(T[] arr) {
-        T[] aux = (T[]) new Comparable[arr.length];
-        sort(arr, aux, 0, arr.length - 1);
+  public static <T extends Comparable<? super T>> void sort(T[] arr) {
+    T[] aux = (T[]) new Comparable[arr.length];
+    sort(arr, aux, 0, arr.length - 1);
+  }
+
+  private static <T extends Comparable<? super T>> void sort(T[] arr, T[] aux, int p, int r) {
+    if (r <= p) return;
+
+    int q = (p + r) >>> 1;
+    sort(arr, aux, p, q);
+    sort(arr, aux, q + 1, r);
+    merge(arr, aux, r, p, q);
+  }
+
+  private static <T extends Comparable<? super T>> void merge(
+      T[] arr, T[] aux, int p, int q, int r) {
+    System.arraycopy(arr, p, aux, p, r - p + 1);
+
+    for (int k = p, i = p, j = q + 1; k <= r; k++) {
+      if (i > q) arr[k] = aux[j++];
+      else if (j > r) arr[k] = aux[i++];
+      else if (aux[j].compareTo(aux[i]) < 0) arr[k] = aux[j++];
+      else arr[k] = aux[i++];
     }
-
-    private static <T extends Comparable<? super T>> void sort(T[] arr, T[] aux, int p, int r) {
-        if (r <= p) return;
-
-        int q = (p + r) >>> 1;
-        sort(arr, aux, p, q);
-        sort(arr, aux, q + 1, r);
-        merge(arr, aux, r, p, q);
-    }
-
-    private static <T extends Comparable<? super T>> void merge(T[] arr, T[] aux, int p, int q, int r) {
-        System.arraycopy(arr, p, aux, p, r - p + 1);
-
-        for (int k = p, i = p, j = q + 1; k <= r; k++) {
-            if (i > q) arr[k] = aux[j++];
-            else if (j > r) arr[k] = aux[i++];
-            else if (aux[j].compareTo(aux[i]) < 0) arr[k] = aux[j++];
-            else arr[k] = aux[i++];
-        }
-    }
+  }
 }
