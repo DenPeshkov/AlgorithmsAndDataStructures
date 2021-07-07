6b98c44cdd4af8c203544c9a3b8986102549f45b
Denis Peshkov <denpeshkov98@gmail.com>
2021-07-07 15:37:34 +0300

    added QuickSelect

---
+++ src/main/java/com/github/denpeshkov/algorithms/selection/QuickSelect.java
@@ -0,0 +1,45 @@
+package com.github.denpeshkov.algorithms.selection;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class QuickSelect {
+  public static <T extends Comparable<? super T>> T select(T[] arr, int k) {
+    if (k < 1 || k > arr.length)
+      throw new IllegalArgumentException("k-th order statistic is not between 1 and " + arr.length);
+    // 0-based indexing
+    k = k - 1;
+
+    int p = 0;
+    int r = arr.length - 1;
+
+    while (r > p) {
+      int q = partition(arr, p, r);
+
+      if (q < k) p = q + 1;
+      else if (q > k) r = q - 1;
+      else return arr[k];
+    }
+    return arr[k];
+  }
+
+  private static <T extends Comparable<? super T>> int partition(T[] arr, int p, int r) {
+    exchange(arr, p, p + ThreadLocalRandom.current().nextInt(r - p + 1));
+
+    int i = p, j = r + 1;
+    T v = arr[p];
+    while (true) {
+      while (arr[++i].compareTo(v) < 0) if (i == r) break;
+      while (arr[--j].compareTo(v) > 0) if (j == p) break;
+      if (i >= j) break;
+      exchange(arr, i, j);
+    }
+    exchange(arr, p, j);
+    return j;
+  }
+
+  private static <T> void exchange(T[] arr, int i, int j) {
+    T temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
