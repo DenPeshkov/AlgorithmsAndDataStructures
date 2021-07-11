bd680761fbc0e712ee4c701fb482cba11c0bc6ad
Denis Peshkov <denpeshkov98@gmail.com>
2021-07-11 17:22:16 +0300

    added InsertionSortSwap

---
+++ src/main/java/com/github/denpeshkov/algorithms/sorting/insertion/InsertionSortSwap.java
@@ -0,0 +1,15 @@
+package com.github.denpeshkov.algorithms.sorting.insertion;
+
+public class InsertionSortSwap {
+  public static <T extends Comparable<? super T>> void sort(T[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) exchange(arr, j, j - 1);
+    }
+  }
+
+  private static <T> void exchange(T[] arr, int i, int j) {
+    T temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
