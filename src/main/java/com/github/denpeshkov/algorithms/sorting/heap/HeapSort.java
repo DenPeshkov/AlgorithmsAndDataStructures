b327d8dca1a43cdf4db83e71b07ecedf00349332
Denis Peshkov <denpeshkov98@gmail.com>
2021-07-12 17:03:35 +0300

    added HeapSort

---
+++ src/main/java/com/github/denpeshkov/algorithms/sorting/heap/HeapSort.java
@@ -0,0 +1,40 @@
+package com.github.denpeshkov.algorithms.sorting.heap;
+
+public class HeapSort {
+  public static <T extends Comparable<? super T>> void sort(T[] arr) {
+    int N = arr.length;
+
+    buildHeap(arr);
+
+    while (N > 1) {
+      exchange(arr, 1, N--);
+      sink(arr, 1, N);
+    }
+  }
+
+  private static <T extends Comparable<? super T>> void buildHeap(T[] arr) {
+    for (int i = arr.length / 2; i >= 1; i--) sink(arr, i, arr.length);
+  }
+
+  private static <T extends Comparable<? super T>> void sink(T[] arr, int i, int N) {
+    while (2 * i <= N) {
+      int max = 2 * i;
+      if (max + 1 <= N && compare(arr, max, max + 1) < 0) max = max + 1;
+      if (compare(arr, i, max) >= 0) break;
+      exchange(arr, i, max);
+      i = max;
+    }
+  }
+
+  // индексация в массиве с 0
+  private static <T extends Comparable<? super T>> int compare(T[] arr, int i, int j) {
+    return arr[i - 1].compareTo(arr[j - 1]);
+  }
+
+  // индексация в массиве с 0
+  private static <T> void exchange(T[] arr, int i, int j) {
+    T temp = arr[i - 1];
+    arr[i - 1] = arr[j - 1];
+    arr[j - 1] = temp;
+  }
+}
