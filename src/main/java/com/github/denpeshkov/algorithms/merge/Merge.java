a8383c817b97aa4243e24a4aef071af02719bde4
Denis Peshkov <denpeshkov98@gmail.com>
2021-07-25 19:07:49 +0300

    added Merge.java

---
+++ src/main/java/com/github/denpeshkov/algorithms/merge/Merge.java
@@ -0,0 +1,22 @@
+package com.github.denpeshkov.algorithms.merge;
+
+public class Merge {
+
+  public static <T extends Comparable<? super T>> T[] merge(T[] a, T[] b) {
+    T[] res = (T[]) new Comparable[a.length + b.length];
+
+    for (int i = 0, j = 0, k = 0; k < res.length; k++) {
+      if (i >= a.length) {
+        res[k] = b[j++];
+      } else if (j >= b.length) {
+        res[k] = a[i++];
+      } else if (b[j].compareTo(a[i]) < 0) {
+        res[k] = b[j++];
+      } else {
+        res[k] = a[i++];
+      }
+    }
+
+    return res;
+  }
+}
