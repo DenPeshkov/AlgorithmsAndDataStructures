9b4d7cd6ba6f24daf8afa8d5015964a3b73f898c
Denis Peshkov <denpeshkov98@gmail.com>
2021-08-15 19:36:56 +0300

    edited merge sort

--- src/main/java/com/github/denpeshkov/algorithms/sorting/merge/MergeSortBottomUp.java
+++ src/main/java/com/github/denpeshkov/algorithms/sorting/merge/MergeSortBottomUp.java
@@ -6,28 +6,7 @@ public class MergeSortBottomUp {
     T[] aux = (T[]) new Comparable[arr.length];
     for (int sz = 1; sz < arr.length; sz *= 2) {
       for (int i = 0; i < arr.length - sz; i += sz * 2) {
-        merge(arr, aux, i, i + sz - 1, Math.min(i + 2 * sz - 1, arr.length - 1));
-      }
-    }
-  }
-
-  // [lo, mid] [mid+1, hi]
-  private static <T extends Comparable<? super T>> void merge(
-      T[] arr, T[] aux, int lo, int mid, int hi) {
-    System.arraycopy(arr, lo, aux, lo, hi - lo + 1);
-
-    int i = lo;
-    int j = mid + 1;
-
-    for (int k = lo; k <= hi; k++) {
-      if (i > mid) {
-        arr[k] = aux[j++];
-      } else if (j > hi) {
-        arr[k] = aux[i++];
-      } else if (aux[j].compareTo(aux[i]) < 0) {
-        arr[k] = aux[j++];
-      } else {
-        arr[k] = aux[i++];
+        MergeSortUtil.merge(arr, aux, i, i + sz - 1, Math.min(i + 2 * sz - 1, arr.length - 1));
       }
     }
   }
--- src/main/java/com/github/denpeshkov/algorithms/sorting/merge/MergeSortTopDown.java
+++ src/main/java/com/github/denpeshkov/algorithms/sorting/merge/MergeSortTopDown.java
@@ -15,28 +15,7 @@ public class MergeSortTopDown {
     int mid = (lo + hi) >>> 1;
     sort(arr, aux, lo, mid);
     sort(arr, aux, mid + 1, hi);
-    merge(arr, aux, hi, lo, mid);
-  }
 
-
-  // [lo, mid] [mid+1, hi]
-  private static <T extends Comparable<? super T>> void merge(
-      T[] arr, T[] aux, int lo, int mid, int hi) {
-    System.arraycopy(arr, lo, aux, lo, hi - lo + 1);
-
-    int i = lo;
-    int j = mid + 1;
-
-    for (int k = lo; k <= hi; k++) {
-      if (i > mid) {
-        arr[k] = aux[j++];
-      } else if (j > hi) {
-        arr[k] = aux[i++];
-      } else if (aux[j].compareTo(aux[i]) < 0) {
-        arr[k] = aux[j++];
-      } else {
-        arr[k] = aux[i++];
-      }
-    }
+    MergeSortUtil.merge(arr, aux, hi, lo, mid);
   }
 }
