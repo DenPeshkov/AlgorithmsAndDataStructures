3d4a16f072bbbddb529f41bbb7a03209b3b0fa80
Denis Peshkov <denpeshkov98@gmail.com>
2021-08-19 19:56:11 +0300

    edited Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+group = "com.github.DenPeshkov"
+version = "1.0-SNAPSHOT"
+description = "AlgorithmsAndDataStructures"
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_16
+java.targetCompatibility = JavaVersion.VERSION_16
